9757ec71f7e0751a04d0de5f8d35e6e36e163b80
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-05-18 12:53:46 +0200

    Feature: Add layouts components with basic styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    PostCardComponent,
+    SubscriptionFormComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'share-your-doubts-angular-firabes-app';
+  title = 'share-your-doubts-angular-firebase-app';
 }
